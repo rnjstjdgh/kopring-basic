76985b895fb1affcfc182ea4f38d139502e885da
rnjstjdgh <gshgsh0831@daum.net>
2022-12-14 01:13:11 +0900

    notes

--- src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
+++ src/main/kotlin/com/group/libraryapp/calculator/Calculator.kt
@@ -1,25 +1,27 @@
 package com.group.libraryapp.calculator
 
 data class Calculator(
-    private var number: Int
+    private var _number: Int
 ) {
+    val number
+        get() = this._number
 
     fun add(operand: Int) {
-        this.number += operand
+        this._number += operand
     }
 
     fun minus(operand: Int) {
-        this.number -= operand
+        this._number -= operand
     }
 
     fun multiply(operand: Int) {
-        this.number *= operand
+        this._number *= operand
     }
 
     fun divide(operand: Int) {
         if(operand == 0) {
             throw IllegalArgumentException("0으로 나눌 수 없습니다")
         }
-        this.number /= operand
+        this._number /= operand
     }
 }
--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,12 +1,20 @@
 package com.group.libraryapp.calculator
 
+import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
 class CalculatorTest {
 
+    @Test
     fun addTest() {
         // given
+        val calculator:Calculator = Calculator(5)
 
         // when
+        calculator.add(3)
 
         // then
+        assertThat(calculator.number).isEqualTo(8)
     }
 }
